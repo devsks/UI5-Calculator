0aaf5460cc7fef2c222c0db651af6b2255b22ff8
I354045 <santosh.kumar.shaw@sap.com>
2018-08-21 17:37:28 +0000

    Code Complete

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,45 +10,107 @@ sap.ui.define([
 				"expression": ""
 			});
 			this.getView().setModel(inputModel, "inputModel");
-			
+
 		},
 		clearInput: function () {
-			this.getView().getModel("inputModel").setProperty("/expression","");
+			this.getView().getModel("inputModel").setProperty("/expression", "");
 		},
-		editInput : function( ch)
-		{
+		editInput: function (ch) {
 			var expression = this.getView().getModel("inputModel").getProperty("/expression");
-			if("+/*-".indexOf(ch)==-1)
-			{
-				expression+=ch;
+			if ("+/*-".indexOf(ch) == -1) {
+				expression += ch;
+			} else {
+				expression += (" " + ch + " ");
 			}
-			else{
-				expression+= (" "+ch+" ");
-			}
-			this.getView().getModel("inputModel").setProperty("/expression",expression);
+			this.getView().getModel("inputModel").setProperty("/expression", expression);
 		},
-		validCompute : function()
-		{
+
+		validCompute: function () {
 			var expression = this.getView().getModel("inputModel").getProperty("/expression");
 			var expList = expression.split(" ");
-			if(isValid(expList) == true)
-			{
-				
-			}
-			else
-			{
+			if (isValid(expList) == true) {
+				var postFix = changeToPostfix(expList);
+				var result = evaluatePostFix(postFix);
+				this.getView().getModel("inputModel").setProperty("/expression", result);
+			} else {
 				alert("Invalid Expression. Please Correct Manually");
 			}
-			
-		},
-		isValid : function(expList)
-		{
-			for(var i = 1; i< expList.length ;++i)
-			{
-				if( ("+-/*".indexOf(expList[i]) != -1) && ("+-/*".indexOf(expList[i]) != -1))
-					return false;
-			}
-			return true;
+
 		}
+
 	});
 });
+var isValid = function (expList) {
+	for (var i = 1; i < expList.length; ++i) {
+		if (("+-/*".indexOf(expList[i - 1]) !== -1) && ("+-/*".indexOf(expList[i]) !== -1))
+			return false;
+	}
+	console.log(expList[expList.length - 1]);
+	return ("+-/*".indexOf(expList[expList.length - 1]) === -1);
+}
+var changeToPostfix = function (expression) {
+
+	var postFix = new Array(); // To store the resultant postFix expression
+	var stack = new Array(); // To Store the operators
+	var precedence = { //Defing the precedence rule
+		"+": 1,
+		"-": 1,
+		"/": 2,
+		"*": 2
+	}
+
+	// Breaking down the expression
+	expression.forEach(element => {
+
+		// It's not a operator
+		if ("+-/*".indexOf(element) == -1) {
+			postFix.push(element);
+		} else // It's a operator
+		{
+			// Checking the precedence and pusing operator into the stack
+			while (stack.length != 0 && precedence[stack[stack.length - 1]] > precedence[element]) {
+				postFix.push(stack.pop());
+			}
+
+			stack.push(element);
+		}
+
+	});
+	// Adding the remaining operators from the stack
+	while (stack.length > 0)
+		postFix.push(stack.pop());
+
+	return postFix;
+}
+var evaluatePostFix = function (postFix) {
+
+	//console.log(line);
+	var stack = Array();
+	postFix.forEach(elements => {
+		if ("+-/*".indexOf(elements) == -1) {
+			stack.push(parseFloat(elements));
+		} else {
+
+			var a = stack.pop(); //2nd Operand
+			var b = stack.pop(); //1st Operand        
+			stack.push(performOperation(b, a, elements));
+		}
+	});
+	// Displaying the result on the webpage.
+	return stack.pop();
+}
+var performOperation = function (a, b, operator) {
+	switch (operator) {
+	case "*":
+		return parseFloat(a) * parseFloat(b);
+	case "-":
+		return parseFloat(a) - parseFloat(b);
+	case "/":
+		return parseFloat(a) / parseFloat(b);
+	case "+":
+		return parseFloat(a) + parseFloat(b);
+	default:
+		alert("Invalid Operation.");
+		return null;
+	}
+}
